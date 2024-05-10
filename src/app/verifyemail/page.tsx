85c2f63b457ccf22ea4891ce2bb82487bb982148
Bablu Kumar Singh <bksingh3520@gmail.com>
2024-05-10 12:02:56 +0530

    added fallback in verify-email page

--- src/app/verifyemail/page.tsx
+++ src/app/verifyemail/page.tsx
@@ -1,43 +1,52 @@
+// VerifyEmail.js
 'use client'
-import React, { useEffect, useState } from 'react'
+import React, { useState, Suspense } from 'react';
 import axios from 'axios';
 import { useRouter, useSearchParams } from 'next/navigation';
 import toast from 'react-hot-toast';
 
+const VerifyEmail = () => {
+  const router = useRouter();
+  const params = useSearchParams();
+  const urlToken = params.get('token');
+  const [token, setToken] = useState(urlToken);
+  const [loading, setLoading] = useState(false);
 
-
-export default function VerifyEmail() {
-  const router = useRouter()
-  const params = useSearchParams()
-  const urlToken = params.get('token')
-  const [token,setToken] = useState(urlToken);
-  const [verified, setVerified] = useState(false)
-  const [loading, setLoading] = useState(false)
-  console.log(token)
-
-
-  const verifyUserEmail = async ()=>{
-    setLoading(true)
+  const verifyUserEmail = async () => {
+    setLoading(true);
     try {
-      const res = await axios.post('/api/users/verifyemail',{token})
-      console.log(res)
-      if(res.data.error){
-        throw new Error(res.data.error)
+      const res = await axios.post('/api/users/verifyemail', { token });
+      if (res.data.error) {
+        throw new Error(res.data.error);
       }
-      toast.success(res.data.message)
-      router.push('/login')
-
+      toast.success(res.data.message);
+      router.push('/login');
     } catch (error:any) {
-      console.log(error.message)
-      toast.error(error.message)
+      toast.error(error.message);
+    } finally {
+      setLoading(false);
     }
-  }
-  
+  };
+
   return (
     <div className='flex items-center justify-center h-screen'>
-      <button onClick={verifyUserEmail} disabled={loading} className='bg-blue-500 text-whiterounded py-2 px-3 rounded-md disabled:opacity-50'>{loading ? 'Verifying...' : 'Verify Email'}</button>
+      <button
+        onClick={verifyUserEmail}
+        disabled={loading}
+        className='bg-blue-500 text-white py-2 px-3 rounded-md disabled:opacity-50'
+      >
+        {loading ? 'Verifying...' : 'Verify Email'}
+      </button>
     </div>
-  )
-}
+  );
+};
 
-  
+const VerifyEmailWithSuspense = () => {
+  return (
+    <Suspense fallback={<div>Loading...</div>}>
+      <VerifyEmail />
+    </Suspense>
+  );
+};
+
+export default VerifyEmailWithSuspense;
